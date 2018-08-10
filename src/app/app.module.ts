eb4eb6fc308b70c36e921422ea8dc590345f8a1a
Raj Rathore <Raj.Rathore@tatacommunications.com>
2018-08-10 12:17:08 +0530

    1. Updated the HTML to use bootstrap. 2. Added a custom pipe 'double' to double the value provided.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {NgModule} from '@angular/core';
 
 
 import {AppComponent} from './app.component';
+import {DoublePipe} from './double.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DoublePipe
   ],
   imports: [
     BrowserModule
