0a01b493c646bc24ad7810d7a56c3b1d4cb23691
Raj Rathore <Raj.Rathore@tatacommunications.com>
2018-08-10 15:16:55 +0530

    1. Made filter pipe impure. 2. Used async pipe.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,9 @@ export class AppComponent {
   myValue = 'lowercase';
   date = new Date(2018, 7, 10);
   myList = ['Milk', 'Bread', 'Beans', 'Banana', 'Honey', 'Mango', 'Egg', 'Raj Rajeshwar Singh Rathore'];
+  asyncValue = new Promise(
+    (resolve, reject) => {
+      setTimeout(() => resolve('Data is here'), 2000);
+    }
+  );
 }
