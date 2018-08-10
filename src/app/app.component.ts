dc32fd2882eb2b7bfcf903eb3f5b6cce0ec9b498
Raj Rathore <Raj.Rathore@tatacommunications.com>
2018-08-10 11:52:45 +0530

    Added some pipes : Date,Uppercase & slice.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  myValue = 'lowercase';
+  date = new Date(2018, 7, 10);
 }
